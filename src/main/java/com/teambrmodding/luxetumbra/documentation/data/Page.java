34bafe40ebfc3b0d2d72aadba102f3e3a72b97ba
Paul Davis <pauldavis101@gmail.com>
2016-10-08 13:04:22 -0500

    Book Work

---
+++ src/main/java/com/teambrmodding/luxetumbra/documentation/data/Page.java
@@ -0,0 +1,15 @@
+package com.teambrmodding.luxetumbra.documentation.data;
+
+/**
+ * This file was created for Lux-et-Umbra-Redux
+ * <p>
+ * Lux-et-Umbra-Redux is licensed under the
+ * Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License:
+ * http://creativecommons.org/licenses/by-nc-sa/4.0/
+ *
+ * @author Paul Davis <pauljoda>
+ * @since 10/6/2016
+ */
+public abstract class Page {
+
+}
